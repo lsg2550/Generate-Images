6b3ceec820ff8923601a0602d29a60b1e2be6cab
Luis <garay_luis@ymail.com>
2016-12-30 19:16:54 -0600

    v1.00
    Known Issues: Memory Leak when building cache

--- src/utils/operations/BuildCache.java
+++ src/utils/operations/BuildCache.java
@@ -29,6 +29,7 @@ public class BuildCache {
 
     private static ImageView toBeGeneratedIV = new ImageView();
     private static HBox imageViewHBox = new HBox(2.5);
+    private static VBox toBeGeneratedVBox = new VBox();
 
     /*START OF BULIDING CACHE*/
     public static void chooseFolder() {
@@ -43,13 +44,13 @@ public class BuildCache {
                 File[] listOfFiles = selectedDirectory.listFiles((File file, String name)
                         -> name.toLowerCase().endsWith(".png") || name.toLowerCase().endsWith(".jpg")
                 );
-                Platform.runLater(() -> {
-                    if (listOfFiles.length > 0) {
-                        displayImagesToBeEdited(splitImagesToRespectiveArrayLists(listOfFiles));
-                    } else {
+                if (listOfFiles.length > 0) {
+                    displayImagesToBeEdited(splitImagesToRespectiveArrayLists(listOfFiles));
+                } else {
+                    Platform.runLater(() -> {
                         new AlertBox("No Images Were Found");
-                    }
-                });
+                    });
+                }
             };
             new Thread(task).start();
         }
@@ -207,73 +208,84 @@ public class BuildCache {
     }
 
     private static void displayImagesToBeEdited(ArrayList<ArrayList<BufferedImage>> splitImages) {
-        GUI.getText().setText("Updating GUI...");
         try {
-            Thread.sleep(100);
+            GUI.getText().setText("Updating GUI...");
+            Thread.sleep(150);  //THREAD
+            Platform.runLater(() -> {
+                toBeGeneratedVBox.getChildren().clear();
+                imageViewHBox.getChildren().clear();
+                toBeGeneratedIV.setImage(null);
+            });
+            /*START: HBOX INSIDE SCROLLPANE*/
+            imageViewHBox.setMaxHeight(5);
+            imageViewHBox.setAlignment(Pos.CENTER);
+
+            //Builds ImageViews to be added to this.HBox
+            for (ArrayList<BufferedImage> splitImage : splitImages) {
+                if (!splitImage.isEmpty()) {
+                    GUI.getText().setText("Updating GUI.");
+                    VBox tempVB = new VBox();
+                    tempVB.setAlignment(Pos.CENTER);
+                    Button left = new Button("<<"), right = new Button(">>");
+                    CheckBox enableDisable = new CheckBox();
+
+                    ImageView imageView = new ImageView(SwingFXUtils.toFXImage(splitImage.get(0), null));
+
+                    GUI.getText().setText("Updating GUI..");
+                    ActionEventHandlers.directionButton(splitImage, imageView, left, right);
+                    ActionEventHandlers.enableDisable(imageView, enableDisable, left, right);
+                    ActionEventHandlers.imageViewListener(imageView);
+
+                    tempVB.getChildren().addAll(left, imageView, right, enableDisable);
+                    imageView.setFitWidth(150);
+                    imageView.setFitHeight(225);
+
+                    GUI.getText().setText("Updating GUI...");
+                    GUI.getpBar().setProgress((double) splitImages.indexOf(splitImage) / splitImages.size());
+
+                    Thread.sleep(150);  //THREAD
+                    Platform.runLater(() -> {
+                        imageViewHBox.getChildren().add(tempVB);
+                    });
+                }
+            }
+
+            ScrollPane sp = new ScrollPane(imageViewHBox);
+            sp.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+            sp.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+            sp.setPannable(true);
+            GUI.getpBar().setProgress(0.60);
+            /*END: HBOX INSIDE SCROLLPANE*/
+
+            //
+            //
+
+            /*START: LONE IMAGEVIEW BELOW SCROLLPANE*/
+            toBeGeneratedVBox.setMaxHeight(225);
+            toBeGeneratedVBox.setAlignment(Pos.CENTER);
+            liveImageBuilding();
+            toBeGeneratedIV.setFitWidth(150);
+            toBeGeneratedIV.setFitHeight(225);
+            Thread.sleep(150);  //THREAD
+            Platform.runLater(() -> {
+                toBeGeneratedVBox.getChildren().addAll(new Text("Image To Be Generated"), toBeGeneratedIV);
+            });
+            GUI.getpBar().setProgress(0.90);
+            /*END: LONE IMAGEVIEW BELOW SCROLLPANE*/
+
+            //Center HBox
+            VBox centerVBox = new VBox(0);
+            VBox.setVgrow(toBeGeneratedVBox, Priority.ALWAYS);
+            centerVBox.setAlignment(Pos.CENTER);
+            Thread.sleep(150);  //THREAD
+            Platform.runLater(() -> {
+                centerVBox.getChildren().addAll(sp, toBeGeneratedVBox);
+                GUI.getRoot().setCenter(centerVBox);
+                GUI.getpBar().setProgress(1.0);
+                GUI.getText().setText("Done!");
+            });
         } catch (InterruptedException ex) {
         }
-        /*START: HBOX INSIDE SCROLLPANE*/
-        imageViewHBox.getChildren().clear();
-        imageViewHBox.setMaxHeight(5);
-        imageViewHBox.setAlignment(Pos.CENTER);
-
-        //Builds ImageViews to be added to this.HBox
-        for (ArrayList<BufferedImage> splitImage : splitImages) {
-            if (!splitImage.isEmpty()) {
-                GUI.getText().setText("Updating GUI.");
-                VBox tempVB = new VBox();
-                tempVB.setAlignment(Pos.CENTER);
-                Button left = new Button("<<"), right = new Button(">>");
-                CheckBox enableDisable = new CheckBox();
-
-                ImageView imageView = new ImageView(SwingFXUtils.toFXImage(splitImage.get(0), null));
-
-                GUI.getText().setText("Updating GUI..");
-                ActionEventHandlers.directionButton(splitImage, imageView, left, right);
-                ActionEventHandlers.enableDisable(imageView, enableDisable, left, right);
-                ActionEventHandlers.imageViewListener(imageView);
-
-                tempVB.getChildren().addAll(left, imageView, right, enableDisable);
-                imageView.setFitWidth(150);
-                imageView.setFitHeight(225);
-
-                GUI.getText().setText("Updating GUI...");
-                GUI.getpBar().setProgress((double) splitImages.indexOf(splitImage) / splitImages.size());
-                imageViewHBox.getChildren().add(tempVB);
-            }
-        }
-
-        ScrollPane sp = new ScrollPane(imageViewHBox);
-        sp.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        sp.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        sp.setPannable(true);
-        GUI.getpBar().setProgress(0.60);
-        /*END: HBOX INSIDE SCROLLPANE*/
-
-        //
-        //
-
-        /*START: LONE IMAGEVIEW BELOW SCROLLPANE*/
-        VBox toBeGeneratedVBox = new VBox();
-        toBeGeneratedVBox.setMaxHeight(225);
-        toBeGeneratedVBox.setAlignment(Pos.CENTER);
-        liveImageBuilding();
-        toBeGeneratedIV.setFitWidth(150);
-        toBeGeneratedIV.setFitHeight(225);
-        toBeGeneratedVBox.getChildren().addAll(new Text("Image To Be Generated"), toBeGeneratedIV);
-        GUI.getpBar().setProgress(0.90);
-        /*END: LONE IMAGEVIEW BELOW SCROLLPANE*/
-
-        //Center HBox
-        VBox centerVBox = new VBox(0);
-        VBox.setVgrow(toBeGeneratedVBox, Priority.ALWAYS);
-        centerVBox.setAlignment(Pos.CENTER);
-        centerVBox.getChildren().addAll(sp, toBeGeneratedVBox);
-        Platform.runLater(() -> {
-            GUI.getRoot().setCenter(centerVBox);
-            GUI.getpBar().setProgress(1.0);
-            GUI.getText().setText("Done!");
-        });
     }/*END OF BULIDING CACHE*/
 
     //
